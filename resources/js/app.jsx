a2de8d65ba1875a0cee0b6154b5dbddcc572a4e0
izzudd <rahmatullohdaffaizzuddin1@gmail.com>
2023-05-20 20:10:12 +0700

    setup inertia with react

---
+++ resources/js/app.jsx
@@ -0,0 +1,21 @@
+import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
+
+import Layout from './layout';
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./pages/**/*.jsx', { eager: true })
+    return pages[`./pages/${name}.jsx`]
+  },
+
+  setup({ el, App, props }) {
+    createRoot(el).render(
+      <Layout>
+        <App {...props} />
+      </Layout>
+    )
+  },
+})
---
+++ resources/js/layout.jsx
@@ -0,0 +1,13 @@
+export default function layout({ children }){
+  return (
+    <div>
+      <header>
+        implement navbar
+      </header>
+      { children }
+      <footer>
+        implement footer
+      </footer>
+    </div>
+  )
+}
