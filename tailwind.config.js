8911297cd8422768b0c4153144ca63468a45998a
izzudd <rahmatullohdaffaizzuddin1@gmail.com>
2023-05-26 15:19:50 +0700

    add neting and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,19 @@ export default {
     extend: {
       colors: {
         'background':'#1C1B1F',
+        'on-background': '#E6E1E5',
         'primary':'#D0BCFF',
         'on-primary':'#381E72',
         'prim-container':'#4F378B',
         'on-prim-container':'#EADDFF',
+        'secondary': '#CCC2DC',
+        'on-secondary': '#332D41',
+        'secondary-container': '#4A4458',
+        'on-secondary-container': '#E8DEF8',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
