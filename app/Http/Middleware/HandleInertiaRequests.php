e95541473861b3925d66ce8109e3c592d3d78731
izzudd <rahmatullohdaffaizzuddin1@gmail.com>
2023-06-07 11:16:13 +0700

    add logos

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,7 +39,10 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'loggedin' => Auth::check(),
-            'user'=> Auth::user()
+            'user'=> Auth::user(),
+            'flash' => [
+                'message' => fn () => $request->session()->get('message')
+            ],
         ]);
     }
 }
